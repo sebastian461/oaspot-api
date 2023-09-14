7faf7a1ebecf4935779132a1c493c3f6828b6626
sebastian461 <sebastiano-461@hotmail.com>
2023-09-13 20:22:38 -0500

    City controller
    The basic functionality to search and create cities was programmed, together with their respective APIs

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -21,8 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 
+/* Authenticate */
 Route::post('register', [AuthController::class, 'register']);
-
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -31,7 +32,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
   /* Authenticate */
   Route::post('logout', [AuthController::class, 'logout']);
+
   /* City */
+  Route::get('city', [CityController::class, 'index']);
+  Route::post('city', [CityController::class, 'store']);
+  Route::get('city/{id}', [CityController::class, 'show']);
 
   /* Country */
   Route::get('country', [CountryController::class, 'index']);
